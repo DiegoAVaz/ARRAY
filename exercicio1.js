e9acb348bda687f5d4ad88420d577afabc0c2d8c
DIEGO FERREIRA ALVES VAZ <diego.favaz@senacsp.edu.br>
2022-06-13 11:47:17 -0300

    first commit

---
+++ exercicio1.js
@@ -0,0 +1,5 @@
+// Pedir um índice para o usuário e retornar o nome correspondente ao índice
+
+let lista = ['Fulano','Ciclano','Beltrano']
+let index = prompt('Digite o índice: ')
+alert(`Você acessou o nome ${lista[index]}`)
---
+++ exercicio2.js
@@ -0,0 +1,27 @@
+/*
+let alunos = ['Amanda','Diego','Caio','Victória','Hugo']
+
+for(let cont = 0; cont < alunos.length; cont++){
+    alert(`Bem-vindo ${alunos[cont]}`)
+}
+*/
+
+// =================================================================================
+
+/*
+const alunos = [
+    {nome: "Ana Paula", idade: 15, sexo: "feminino"},
+    {nome: "Carlos", idade: 17, sexo: "masculino"},
+    {nome: "Cristiano", idade: 14, sexo: "masculino"},
+    {nome: "Aline", idade: 13, sexo: "feminino"},
+    {nome: "Pedro", idade: 15, sexo: "masculino"},
+    {nome: "Sofia", idade: 12, sexo: "feminino"},
+    {nome: "Paloma", idade: 15, sexo: "feminino"}
+]
+
+console.log(alunos.filter(function(p){
+    if(p.idade >= 15 && p.idade <= 17){
+        return p
+    }
+}))
+*/
